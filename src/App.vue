4c9ca02530c117f4aa1c809cafb2e6718122d8c8
Amir Ali anwar <amiralianwar611@gmail.com>
2023-02-23 08:58:02 +0500

    inital project setup

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+<div id="app">
+
+    <h1>Hi There</h1>
+</div>
+</template>
+
+    
+<script>
+</script>
+    
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+    box-sizing: border-box;
+}
+
+html {
+    font-family: 'Roboto', sans-serif;
+}
+
+body {
+    margin: 0;
+}
+</style>
